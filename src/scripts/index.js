814d121fc888649710fc38d38bfcbff5fb9a9944
Bogdan Gaevskij <liltracyn@yandex.ru>
2024-10-24 16:59:49 +0300

    minor chanches

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,8 @@ import {initialCards} from './cards.js'
 
 const cardTemplate = document.querySelector("#card-template").content;
 const cardList = document.querySelector(".places__list");
+const profileSection = document.querySelector('.profile')
+
 
 function createCard(template, arrayItem) {
   const card = template.querySelector(".places__item").cloneNode(true);
@@ -29,3 +31,14 @@ initialCards.forEach((item) => {
   const card = createCard(cardTemplate, item);
   cardList.append(card);
 });
+
+profileSection.addEventListener('click',(e)=>{
+  
+  // if(e.target.classList.contains('profile__edit-button')){
+  //   const popUpEdit = document.querySelector('.popup_type_edit')
+  //   popUpEdit.classList.add('popup_is-opened')
+  // } else if ( e.target.classList.contains('profile__add-button')){
+  //   const popUpEdit = document.querySelector('.popup_type_new-card')
+  //   popUpEdit.classList.add('popup_is-opened')
+  // }
+})
