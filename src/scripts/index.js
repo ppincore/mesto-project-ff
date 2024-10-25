13ee588723a911131cc2f8920c314007f861c970
Bogdan Gaevskij <liltracyn@yandex.ru>
2024-10-25 15:27:46 +0300

    add: function open Image Popup

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,47 +3,62 @@ import {initialCards} from './cards.js'
 
 const cardTemplate = document.querySelector("#card-template").content;
 const cardList = document.querySelector(".places__list");
-const profileSection = document.querySelector('.profile')
-const profileSectionButtons = profileSection.querySelectorAll('button')
-const popupList = document.querySelectorAll('.popup')
-console.log(popupList)
-const closeBtn = document.querySelectorAll('.popup__close')
 
+const profileSection = document.querySelector('.profile')
+
+profileSection.addEventListener('click',(e)=>{console.log(e.target)})
+
+
+const popupList = document.querySelectorAll('.popup')
+const popupArray = Array.from(popupList)
+// создание карточки
 function createCard(template, arrayItem) {
   const card = template.querySelector(".places__item").cloneNode(true);
-
-  const deleteButton = card.querySelector(".card__delete-button");
-  deleteButton.addEventListener("click", deleteCard);
-
   const cardTitle = card.querySelector(".card__title");
+
   const cardImage = card.querySelector(".card__image");
+  const deleteButton = card.querySelector(".card__delete-button");
+  
   cardTitle.textContent = arrayItem.name;
   cardImage.setAttribute("src", arrayItem.link);
   cardImage.setAttribute("alt", arrayItem.name);
 
+  deleteButton.addEventListener("click", deleteCard);
+  cardImage.addEventListener('click',openPopupImage)
+
   return card;
 }
-
+// удаление карточки
 function deleteCard(e) {
   let target = e.target;
   let deletedCard = target.closest(".places__item");
   deletedCard.remove();
 }
+//функция открытия попап изображения
+function openPopupImage(e){
+  const popupImage = popupArray.find((popup)=>{
+   return popup.classList.contains('popup_type_image')
+  })
+  popupImage.querySelector('.popup__image').src = e.target.src
+  popupImage.classList.add('popup_is-opened')
+}
 
+
+
+// добавление карточек на страницу
 initialCards.forEach((item) => {
   const card = createCard(cardTemplate, item);
   cardList.append(card);
 });
 
-closeBtn.forEach((i)=>{
-  i.addEventListener('click',(e)=>{
-    e.target.closest('.popup').classList.remove('popup_is-opened')
-  })
- })
 
-//  function openPopup(target){
-//   let popup = document.querySelector(`.${target}`)
-//   popup.classList.add('popup_is-opened')
-// }
 
-// openPopup('popup_type_edit')
+const p = document.querySelector('.popup_type_new-card')
+.addEventListener('click',(e)=>{
+  
+  if(e.target.contains('.popup'))
+  {
+    
+  }
+
+})
