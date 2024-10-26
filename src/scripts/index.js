506a69dbe2f8bb26526945c15964ba8470abd143
Bogdan Gaevskij <liltracyn@yandex.ru>
2024-10-26 13:28:11 +0300

    add: function open and close all popups on page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,49 +1,13 @@
-import '../pages/index.css'
-import {initialCards} from './cards.js'
+import '../pages/index.css';
+import { initialCards } from './cards.js';
 
-const cardTemplate = document.querySelector("#card-template").content;
-const cardList = document.querySelector(".places__list");
+const cardTemplate = document.querySelector('#card-template').content;
+const cardList = document.querySelector('.places__list');
 
-const profileSection = document.querySelector('.profile')
+const profileSection = document.querySelector('.profile');
 
-profileSection.addEventListener('click',(e)=>{console.log(e.target)})
-
-
-const popupList = document.querySelectorAll('.popup')
-const popupArray = Array.from(popupList)
-// создание карточки
-function createCard(template, arrayItem) {
-  const card = template.querySelector(".places__item").cloneNode(true);
-  const cardTitle = card.querySelector(".card__title");
-
-  const cardImage = card.querySelector(".card__image");
-  const deleteButton = card.querySelector(".card__delete-button");
-  
-  cardTitle.textContent = arrayItem.name;
-  cardImage.setAttribute("src", arrayItem.link);
-  cardImage.setAttribute("alt", arrayItem.name);
-
-  deleteButton.addEventListener("click", deleteCard);
-  cardImage.addEventListener('click',openPopupImage)
-
-  return card;
-}
-// удаление карточки
-function deleteCard(e) {
-  let target = e.target;
-  let deletedCard = target.closest(".places__item");
-  deletedCard.remove();
-}
-//функция открытия попап изображения
-function openPopupImage(e){
-  const popupImage = popupArray.find((popup)=>{
-   return popup.classList.contains('popup_type_image')
-  })
-  popupImage.querySelector('.popup__image').src = e.target.src
-  popupImage.classList.add('popup_is-opened')
-}
-
-
+const popupList = document.querySelectorAll('.popup');
+const popupArray = Array.from(popupList);
 
 // добавление карточек на страницу
 initialCards.forEach((item) => {
@@ -51,14 +15,87 @@ initialCards.forEach((item) => {
   cardList.append(card);
 });
 
+// СЛУШАТЕЛИ СОБЫТИЙ
 
+// прослушиватель на секциюю
+profileSection.addEventListener('click', openPopup);
 
-const p = document.querySelector('.popup_type_new-card')
-.addEventListener('click',(e)=>{
-  
-  if(e.target.contains('.popup'))
-  {
-    
+// ФУНКЦИИ
+
+// создание карточки
+function createCard(template, arrayItem) {
+  const card = template.querySelector('.places__item').cloneNode(true);
+  const cardTitle = card.querySelector('.card__title');
+
+  const cardImage = card.querySelector('.card__image');
+  const deleteButton = card.querySelector('.card__delete-button');
+
+  cardTitle.textContent = arrayItem.name;
+  cardImage.setAttribute('src', arrayItem.link);
+  cardImage.setAttribute('alt', arrayItem.name);
+
+  deleteButton.addEventListener('click', deleteCard);
+  cardImage.addEventListener('click', openPopup);
+
+  return card;
+}
+
+// удаление карточки
+function deleteCard(e) {
+  let target = e.target;
+  let deletedCard = target.closest('.places__item');
+  deletedCard.remove();
+}
+
+// функция откртытия попапа
+function openPopup(e) {
+  let target = e.target;
+  let popup = null;
+  const obj = {
+    'profile__edit-button': 'popup_type_edit',
+    'profile__add-button': 'popup_type_new-card',
+    card__image: 'popup_type_image',
+  };
+  for (let key in obj) {
+    if (target.classList.contains(key)) {
+      popup = popupArray.find((i) => i.classList.contains(obj[key]));
+      if (popup) {
+        if (popup.classList.contains('popup_type_image')) {
+          popup.querySelector('.popup__image').src = e.target.src;
+        }
+        popup.classList.add('popup_is-opened');
+        document.addEventListener('keydown', closeEsc)
+        popup.addEventListener('click',closePopup)
+      }
+      break;
+    }
   }
+  
+}
+// функция закрытия попапа
+function closePopup(e) {
+  if (
+    e.target.classList.contains('popup') ||
+    e.target.classList.contains('popup__close')
+  ) {
+    e.target.closest('.popup').classList.remove('popup_is-opened');
+  }
+  document.removeEventListener('keydown',closeEsc)
+}
 
-})
+// функция закрытия попап по esc
+function closeEsc(e) {
+  if (e.key.toLowerCase() === 'escape') {
+    const closePopup = popupArray.find((popup) => {
+      return popup.classList.contains('popup_is-opened');
+    });
+    if (closePopup) {
+      closePopup.classList.remove('popup_is-opened');
+    }
+  }
+}
+
+
+
+
+
