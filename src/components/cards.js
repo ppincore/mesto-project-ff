55bac0c4ee685733a0c054340111171a43e7c885
Bogdan Gaevskij <liltracyn@yandex.ru>
2024-10-28 11:56:57 +0300

    delete comments and prettier

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,67 +1,61 @@
-import {openModal} from '../components/modal.js'
+import { openModal } from '../components/modal.js';
 
 const initialCards = [
-    {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    },
-    {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-    },
-    {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-    },
-    {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-    },
-    {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-    },
-    {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    }
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+  },
 ];
 
-
-
-// создание карточки
 function createCard(template, arrayItem) {
   const card = template.querySelector('.places__item').cloneNode(true);
   const cardTitle = card.querySelector('.card__title');
 
   const cardImage = card.querySelector('.card__image');
   const deleteButton = card.querySelector('.card__delete-button');
-  const likeButton = card.querySelector('.card__like-button')
+  const likeButton = card.querySelector('.card__like-button');
 
   cardTitle.textContent = arrayItem.name;
   cardImage.setAttribute('src', arrayItem.link);
   cardImage.setAttribute('alt', arrayItem.name);
-  
+
   deleteButton.addEventListener('click', deleteCard);
   cardImage.addEventListener('click', openModal);
-  likeButton.addEventListener('click',cardLike)
+  likeButton.addEventListener('click', cardLike);
 
   return card;
 }
 
-// удаление карточки
 function deleteCard(e) {
   let target = e.target;
   let deletedCard = target.closest('.places__item');
   deletedCard.remove();
 }
 
-// функция лайка для карточки
-function cardLike(e){
-  if(e.target.classList.contains('card__like-button')){
-    e.target.classList.toggle('card__like-button_is-active')
+function cardLike(e) {
+  if (e.target.classList.contains('card__like-button')) {
+    e.target.classList.toggle('card__like-button_is-active');
   }
 }
 
-
-export {initialCards,createCard}
+export { initialCards, createCard };
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,6 @@
 import '../pages/index.css';
-import {initialCards,createCard} from '../components/cards.js';
-import {openModal,handleFormSubmit, addCard} from '../components/modal.js'
-
+import { initialCards, createCard } from '../components/cards.js';
+import { openModal, handleFormSubmit, addCard } from '../components/modal.js';
 
 const cardTemplate = document.querySelector('#card-template').content;
 const cardList = document.querySelector('.places__list');
@@ -11,51 +10,38 @@ const profileSection = document.querySelector('.profile');
 const popupList = document.querySelectorAll('.popup');
 const popupArray = Array.from(popupList);
 
-popupArray.forEach((i)=>i.classList.add('popup_is-animated'))
+popupArray.forEach((i) => i.classList.add('popup_is-animated'));
 
-const formElement  = document.forms['edit-profile']
-const nameInput = formElement.querySelector('.popup__input_type_name')
-const jobInput = formElement.querySelector('.popup__input_type_description')
+const formElement = document.forms['edit-profile'];
+const nameInput = formElement.querySelector('.popup__input_type_name');
+const jobInput = formElement.querySelector('.popup__input_type_description');
 
-const profileTitle = profileSection.querySelector('.profile__title')
-const profileDescription = profileSection.querySelector('.profile__description')
+const profileTitle = profileSection.querySelector('.profile__title');
+const profileDescription = profileSection.querySelector(
+  '.profile__description'
+);
 
-const formAddCard = document.forms['new-place']
-const cardNamePlace = formAddCard.querySelector('.popup__input_type_card-name')
-const cardImageLink = formAddCard.querySelector('.popup__input_type_url')
+const formAddCard = document.forms['new-place'];
+const cardNamePlace = formAddCard.querySelector('.popup__input_type_card-name');
+const cardImageLink = formAddCard.querySelector('.popup__input_type_url');
 
-// добавление карточек на страницу
 initialCards.forEach((item) => {
   const card = createCard(cardTemplate, item);
   cardList.append(card);
 });
 
-// СЛУШАТЕЛИ СОБЫТИЙ
-
-// прослушиватель на секциюю
 profileSection.addEventListener('click', openModal);
-// просшуливатель на форму профиля
-formElement.addEventListener('submit', handleFormSubmit); 
-// прослушиватель на форму добавления карточки
-formAddCard.addEventListener('submit',addCard)
-// ФУНКЦИИ
 
+formElement.addEventListener('submit', handleFormSubmit);
 
+formAddCard.addEventListener('submit', addCard);
 
-export {nameInput,
-    popupArray,
-    jobInput,
-    profileTitle,
-    profileDescription,
-    cardNamePlace,
-    cardImageLink,
-}
-
-
-
-
-
-
-
-
-
+export {
+  nameInput,
+  popupArray,
+  jobInput,
+  profileTitle,
+  profileDescription,
+  cardNamePlace,
+  cardImageLink,
+};
