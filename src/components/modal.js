08266d3390965998acee5c6e3092057f9704bf6f
Bogdan Gaevskij <liltracyn@yandex.ru>
2024-10-30 16:20:03 +0300

    -

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,21 +1,22 @@
 function openModal(popup){
-
   popup.classList.add('popup_is-opened')
-  popup.addEventListener('mousedown',()=>closePopup(popup))
-  document.addEventListener('keydown',()=>closePopup(popup))
+  popup.addEventListener('click', closePopup);
+  document.addEventListener('keydown', closePopup);
 }
 
 
-function closePopup(popup) {
-  popup.classList.remove('popup_is-opened')
-  console.log(popup)
-  popup.removeEventListener('mousedown',close)
-  document.removeEventListener('keydown', closeModalEsc);
+function closePopup(e) {
+  const popup = document.querySelector('.popup_is-opened');
+  const target = e.target
+  if(e.key ==='Escape'){
+    popup.classList.remove('popup_is-opened');
+    popup.removeEventListener('click', closePopup);
+  }
+  if(target.classList.contains('popup__close') || e.target === popup){
+    popup.classList.remove('popup_is-opened');
+    document.removeEventListener('keydown', closePopup);
+  }
 }
 
-function closeModalEsc() {
 
-
-}
-
-export {closePopup,closeModalEsc,openModal}
+export {closePopup,openModal}
