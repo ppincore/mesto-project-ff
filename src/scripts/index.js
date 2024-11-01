8c29d7043be86f2911fed950792841dc8b7d74f9
Bogdan Gaevskij <liltracyn@yandex.ru>
2024-11-01 14:06:24 +0300

    minor chanches

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -63,6 +63,7 @@ function editProfileSection(e) {
   e.preventDefault();
   profileTitle.textContent = nameInput.value;
   profileDescription.textContent = jobInput.value;
+  formEditProfile.reset();
 }
 
 function addNewCard(e) {
