8a83f4cf6e4943667ba434c0c09205796feecc83
Bogdan Gaevskij <liltracyn@yandex.ru>
2024-11-02 21:53:58 +0300

    refactor functions

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,29 +1,16 @@
 function openModal(popup) {
-  popup.classList.add('popup_is-animated');
-  setTimeout(() => {
-    popup.classList.add('popup_is-opened');
-  }, 0);
-  popup.addEventListener('click', closePopup);
-  document.addEventListener('keydown', closePopup);
+  popup.classList.add('popup_is-opened');
+  document.addEventListener('keydown', closeEscPopup);
 }
 
-function closePopup(e) {
-  const target = e.target;
-  const popupToClose = document.querySelector('.popup_is-opened');
-  if (
-    e.key === 'Escape' ||
-    target.type === 'submit' ||
-    target.classList.contains('popup__close') ||
-    target === popupToClose
-  ) {
-    popupToClose.classList.remove('popup_is-opened');
-    setTimeout(() => {
-      popupToClose.classList.remove('popup_is-animated');
-    }, 600);
-    popupToClose.querySelector('form').reset();
-    popupToClose.removeEventListener('click', closePopup);
-    document.removeEventListener('keydown', closePopup);
+function closeModal(popup) {
+  popup.classList.remove('popup_is-opened');
+  document.removeEventListener('keydown', closeEscPopup);
+}
+function closeEscPopup(e) {
+  if (e.key === 'Escape') {
+    const popup = document.querySelector('.popup_is-opened');
+    closeModal(popup);
   }
 }
-
-export { openModal };
+export { openModal, closeModal };
