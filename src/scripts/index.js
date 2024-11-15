2a32566eda4c93d19ba859f190c0646a98b4dea5
Bogdan Gaevskij <liltracyn@yandex.ru>
2024-11-15 11:49:17 +0300

    add: attributes for inputs

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -26,13 +26,35 @@ const popupImageCaption = popupTypeImage.querySelector('.popup__caption');
 // Форма 1
 const formEditProfile = document.forms['edit-profile'];
 const nameInput = formEditProfile.querySelector('.popup__input_type_name');
+nameInput.setAttribute('minlength', '2')
+nameInput.setAttribute('mixlength', '40')
 const jobInput = formEditProfile.querySelector(
   '.popup__input_type_description'
 );
+
+jobInput.setAttribute('minlength', '2')
+jobInput.setAttribute('mixlength', '200')
+
+// эл-ы ошибок Форма 1
+
+const nameErrorMessage = formEditProfile.querySelector(
+  `.${nameInput.id}-error`
+);
+const jobErrorMessage = formEditProfile.querySelector(`.${jobInput.id}-error`);
+
 // Форма 2
 const formAddCard = document.forms['new-place'];
 const cardNamePlace = formAddCard.querySelector('.popup__input_type_card-name');
+cardNamePlace.setAttribute('minlength', '2')
+cardNamePlace.setAttribute('mixlength', '30')
 const cardImageLink = formAddCard.querySelector('.popup__input_type_url');
+// эл-ы ошибок Форма 2
+const cardNameErrorMessage = formAddCard.querySelector(
+  `.${cardNamePlace.id}-error`
+);
+const cardImageErrorMessage = formAddCard.querySelector(
+  `.${cardImageLink.id}-error`
+);
 
 function editProfileSection(e) {
   e.preventDefault();
